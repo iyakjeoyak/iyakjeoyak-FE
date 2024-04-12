fcbf2c5bac21e6a6efa53ca6b1f7cb70e16b6091
hyunjinb <hyunjinb394@gmail.com>
2024-04-13 02:09:07 +0900

    Env : vite-ts 환경 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,54 @@
-import { defineConfig } from 'vite'
-import * as path from "path"
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import svgrPlugin from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import * as path from "path";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgrPlugin()],
+	plugins: [react(), tsconfigPaths(), svgrPlugin()],
 
+	server: {
+		port: 3000,
+	},
 
-  server: {
-    port: 3000,
-  },
-  
-  build: {
-    // 빌드할 라이브러리에 대한 설정
-    lib: {
-      // 라이브러리의 진입점
-      entry: path.resolve(__dirname, "src/index.tsx"),
-      // 라이브러리 이름
-      name: "index",
-      fileName: "index",
-    },
-    rollupOptions: {
-      // 번들에 포함시키지 않을 외부 종속성
-      external: ["react"],
-      // 번들의 출력 옵션 설정
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
-    // CommonJS 번들러에 대한 옵션을 정의한다.
-    commonjsOptions: {
-      esmExternals: ["react"],
-    },
-  },
-  
-  // 절대경로
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets" : path.resolve(__dirname, './src/assets'),
-      "@components" : path.resolve(__dirname, './src/components'),
-      // "@components" : path.resolve(__dirname, './src/components'),
+	build: {
+		// 빌드할 라이브러리에 대한 설정
+		lib: {
+			// 라이브러리의 진입점
+			entry: path.resolve(__dirname, "src/index.tsx"),
+			// 라이브러리 이름
+			name: "index",
+			fileName: "index",
+		},
+		rollupOptions: {
+			// 번들에 포함시키지 않을 외부 종속성
+			external: ["react"],
+			// 번들의 출력 옵션 설정
+			output: {
+				globals: {
+					react: "React",
+				},
+			},
+		},
+		// CommonJS 번들러에 대한 옵션을 정의한다.
+		commonjsOptions: {
+			esmExternals: ["react"],
+		},
+	},
 
-
-    },
-  },
-
-})
-
-
+	// 절대경로
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "src"),
+			"@assets": path.resolve(__dirname, "src/assets"),
+			"@components": path.resolve(__dirname, "src/components"),
+			"@hooks/*": path.resolve(__dirname, "src/hooks"),
+			"@pages/*": path.resolve(__dirname, "src/pages"),
+			"@router/*": path.resolve(__dirname, "src/router"),
+			"@stories/*": path.resolve(__dirname, "src/stories"),
+			"@styles/*": path.resolve(__dirname, "src/styles"),
+			"@utils/*": path.resolve(__dirname, "src/utils"),
+		},
+	},
+});
