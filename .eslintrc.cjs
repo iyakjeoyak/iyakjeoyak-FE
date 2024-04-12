cea6203591eccd155f875e03b5bac9e7c8993d90
hyunjinb <hyunjinb394@gmail.com>
2024-04-13 00:40:48 +0900

    Chore : vite-ts 절대경로 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,8 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
