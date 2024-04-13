cb77f2dea64ea3127da2d3aa8d27b23e3f600944
a-honey <jungahyeon0512@khu.ac.kr>
2024-04-13 14:52:25 +0900

    fix: change routing

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,16 +5,16 @@ import Layout from "../pages/layout";
 import LoginPage from "../pages/login";
 
 function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Layout />}>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/login" element={<LoginPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default Router;
