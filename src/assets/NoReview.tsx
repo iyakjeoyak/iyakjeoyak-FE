3046d11d148621f194687de56b1f4115365e288c
hyunjinb <hyunjinb394@gmail.com>
2024-04-15 15:15:30 +0900

    Design : svg icon 추가

---
+++ src/assets/NoReview.tsx
@@ -0,0 +1,43 @@
+import { FC, SVGProps } from "react";
+
+const NoReview: FC<SVGProps<SVGSVGElement>> = (props) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width={props.width || "200"}
+			height={props.height || "200"}
+			fill="none"
+			viewBox="0 0 57 34"
+			{...props}
+		>
+			<path
+				fill='url("#SvgjsLinearGradient2050")'
+				d="M28.3 31.1c-19.8 6.6-35.8-2.6-24.4-14C-7.5 5.7 8.5-3.5 28.3 3c19.8-6.6 35.8 2.6 24.4 14 11.4 11.4-4.6 20.7-24.4 14.1Z"
+			></path>
+			<defs>
+				<linearGradient
+					gradientTransform="rotate(90 0.5 0.5)"
+					id="SvgjsLinearGradient2050"
+				>
+					<stop
+						stop-opacity="1"
+						stop-color="rgba(35, 204, 135)"
+						offset="0.00578125"
+					></stop>
+					<stop
+						stop-opacity="1"
+						stop-color="rgba(234, 204, 248)"
+						offset="0.48"
+					></stop>
+					<stop
+						stop-opacity="1"
+						stop-color="rgba(34, 176, 190)"
+						offset="1"
+					></stop>
+				</linearGradient>
+			</defs>
+		</svg>
+	);
+};
+
+export default NoReview;
