6425a36c442d31eeb5a058091f20c275bf0702ed
a-honey <jungahyeon0512@khu.ac.kr>
2024-04-17 02:54:04 +0900

    Feat: 영양제 조회 페이지 컴포넌트 분리

--- src/pages/detail/UI/MedicineCard.tsx
+++ src/pages/detail/UI/MedicineCard.tsx
@@ -1,6 +1,8 @@
+import styles from "../styles/MedicineCard.module.scss";
+
 export default function MedicineCard() {
 	return (
-		<div>
+		<div className={styles.container}>
 			<div>이미지</div>
 			<div>어쩌구 영양제</div>
 		</div>
--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,10 +1,21 @@
-import { InfoBoard, MedicineCard } from "./UI";
+import { InfoBoard, MedicineCard, ReviewBoard } from "./UI";
+
+import SelectSort from "@/components/SelectSort";
+import TabBar from "@/components/TabBar";
+import styles from "./index.module.scss";
 
 export default function DetailIdPage() {
 	return (
-		<section>
-			<MedicineCard />
-			<InfoBoard />
-		</section>
+		<>
+			<section className={styles.container}>
+				<MedicineCard />
+				<div className={styles.board}>
+					<TabBar />
+					<InfoBoard />
+					<SelectSort />
+					<ReviewBoard />
+				</div>
+			</section>
+		</>
 	);
 }
