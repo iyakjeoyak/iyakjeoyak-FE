f82b1781c05bc136f85359e6a339fa9812aa83e0
a-honey <jungahyeon0512@khu.ac.kr>
2024-04-17 13:37:59 +0900

    Feat: 디테일 페이지 마크업

--- src/components/WriterTitle/index.tsx
+++ src/components/WriterTitle/index.tsx
@@ -6,7 +6,7 @@ export default function WriterTitle() {
 			<img />
 			<div className={styles["content-container"]}>
 				<div className={styles.star}>
-					별별별별별<span>4</span>
+					★★★★☆<span>4</span>
 				</div>
 				<div className={styles.writer}>
 					작성자<span>2024.04.17</span>
---
+++ src/pages/detail/UI/InfoBoardItemContainer.tsx
@@ -0,0 +1,23 @@
+import styles from "../styles/InfoBoardItemContainer.module.scss";
+
+interface InfoBoardItemContainerProps {
+	title: string;
+	children: React.ReactNode;
+	isMoreBtn?: boolean;
+}
+
+export default function InfoBoardItemContainer({
+	title,
+	children,
+	isMoreBtn = false,
+}: InfoBoardItemContainerProps) {
+	return (
+		<div className={styles.container}>
+			<div>{title}</div>
+			<div className={styles["content-container"]}>
+				<p className={styles.description}>{children}</p>
+				{isMoreBtn && <button>more</button>}
+			</div>
+		</div>
+	);
+}
