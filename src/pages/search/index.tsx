1062f059c52301447d1d666909b7ea6af4f0a045
a-honey <jungahyeon0512@khu.ac.kr>
2024-04-17 13:53:45 +0900

    Feat: add TapBar

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,12 +1,29 @@
 import { MedicineCardList } from "@/pages/search/UI";
 import SearchBar from "@/components/SearchBar";
-import TabBar from "@/components/TabBar";
+import TapBar from "@/components/TapBar";
+import { useState } from "react";
+
+const TABS = [
+	{ name: "전체", value: "all" },
+	{ name: "기능별", value: "feature" },
+	{ name: "성능별", value: "ingredient" },
+];
 
 export default function SearchPage() {
+	const [currentTapValue, setCurrentTapValue] = useState("all");
+
+	const handleChangeCurrentTapValue = (tapValue: string) => {
+		setCurrentTapValue(tapValue);
+	};
+
 	return (
 		<section>
 			<SearchBar />
-			<TabBar />
+			<TapBar
+				taps={TABS}
+				currentTapValue={currentTapValue}
+				handleChangeCurrentTapValue={handleChangeCurrentTapValue}
+			/>
 			<MedicineCardList />
 		</section>
 	);
