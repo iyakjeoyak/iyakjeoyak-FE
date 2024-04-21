9528852fc51fad31be2d2b7f435a90bc3c5f9bde
hyunjinb <hyunjinb394@gmail.com>
2024-04-22 08:10:10 +0900

    Refactor : Empty 컴포넌트 따로 빼둠

--- src/pages/userinfo/UI/ReviewEmpty.tsx
+++ src/pages/userinfo/UI/ReviewEmpty.tsx
@@ -1,26 +1,23 @@
 import "@styles/global.scss";
-// import { userMockData, supplementRecordMockData } from "../mockData";
-import style from "../style/reviewempty.module.scss";
 import { Button } from "@/components/Button";
 import { useNavigate } from "react-router-dom";
+import { Empty } from "@/pages/feedback/UI";
 
 const ReviewEmpty = () => {
 	const navigate = useNavigate();
 	return (
-		<section className={style.noReviewContainer}>
-			<div className={style.noReviewContent}>
-				<div className={style.noReviewTitle}>아직 작성된 후기가 없습니다</div>
-				<Button
-					name="이번 주 BEST 영양제 확인하기"
-					type="button"
-					variant="dark"
-					size="medium"
-					onClick={async () => {
-						navigate(`/`);
-					}}
-				/>
-			</div>
-		</section>
+		<Empty description="아직 작성된 후기가 없습니다">
+			<Button
+				type="button"
+				variant="dark"
+				size="m"
+				onClick={() => {
+					navigate(`/home`);
+				}}
+			>
+				이번 주 BEST 영양제 확인하기{" "}
+			</Button>
+		</Empty>
 	);
 };
 
