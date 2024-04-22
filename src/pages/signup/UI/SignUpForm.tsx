4fbb844eacd5e98c384d238f80e1ea9aecc40f10
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2024-04-22 15:47:05 +0900

    fix: Login, SignUp 페이지 작업

--- src/pages/signup/UI/SignUpForm.tsx
+++ src/pages/signup/UI/SignUpForm.tsx
@@ -1,127 +1,171 @@
 import { Form } from "@/components/Form";
-import TagCommon from "@/components/Tag";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { SubmitHandler, useForm } from "react-hook-form";
-import * as yup from "yup";
+import { useFormContext } from "react-hook-form";
+import { signUpDefault, useSignupValidation } from "./signupValidation";
+import { ErrorMessage } from "@hookform/error-message";
 import { DevTool } from "@hookform/devtools";
+import { tagData } from "../../../components/Form/TagButton/TagData";
+import TagButton from "@/components/Form/TagButton";
+import styles from "@/pages/signup/style/SignUpForm.module.scss";
+import { ImgInput } from "@/components/Form/ImgInput";
 
-interface SignUpData {
+export interface SignUpType {
+	profileImage: File;
 	username: string; // 아이디
 	password: string; // 비밀번호
 	confirmPassword: string; // 비밀번호 검사
 	nickname: string; // 닉네임
 	gender: string; // 성별
-	age?: number | undefined | null;
-	tag: (string | undefined)[]; // 태그
+	age: number | undefined | null; // 나이
+	tag: []; //태그
 }
 export function SignUpForm() {
-	const schema = yup.object().shape({
-		username: yup
-			.string()
-			.min(6, "아이디는 6자리 이상이어야합니다.")
-			.required("아이디를 입력하세요."),
-		password: yup
-			.string()
-			.min(8, "비밀번호는 8자리 이상이어야 합니다.")
-			.required("비밀번호를 입력하세요."),
-		confirmPassword: yup
-			.string()
-			.oneOf([yup.ref("password"), ""], "비밀번호가 일치하지 않습니다.")
-			.required("비밀번호를 입력하세요."),
-		nickname: yup.string().required("닉네임을 입력하세요."),
-		gender: yup.string().required("성별을 선택하세요."),
-		age: yup
-			.number()
-			.transform((value) =>
-				isNaN(value) || value === null || value === undefined ? 0 : value,
-			)
-			.integer("나이를 정수로 입력하세요.")
-			.positive("나이는 양수여야 합니다.")
-			.required("나이를 입력하세요."),
-		tag: yup.array().of(yup.string()).required("태그를 입력하세요."),
-	});
+	return (
+		// 로그인 유효성 검사
+		<Form
+			validationSchema={useSignupValidation()}
+			pageDefaultValues={signUpDefault}
+		>
+			<SignUpInput />
+		</Form>
+	);
+}
+
+function SignUpInput() {
 	const {
-		register,
 		handleSubmit,
 		formState: { errors },
 		control,
-	} = useForm<SignUpData>({
-		resolver: yupResolver<SignUpData>(schema),
-		defaultValues: {
-			gender: "남성",
-		},
-	});
+	} = useFormContext<SignUpType>();
 
-	const onSubmit: SubmitHandler<SignUpData> = async (formData) => {
-		// console.log(formData);
+	const onSubmit = async (data: SignUpType) => {
+		console.log("data", data);
 	};
+
 	return (
 		<div>
-			<div>성별</div>
-
-			<Form schema={schema} onSubmit={handleSubmit(onSubmit)}>
-				<Form.Input
-					text="아이디"
-					type="text"
-					placeholder="아이디를 입력해주세요."
-					{...register("username", { required: true })}
-				/>
-				{errors.username && <p className="error">{errors.username.message}</p>}
-				<Form.Input
-					text="비밀번호"
-					type="password"
-					placeholder="비밀번호를 입력해주세요."
-					{...register("password", { required: true })}
-				/>
-				{errors.password && <p className="error">{errors.password.message}</p>}
-				<Form.Input
-					text="비밀번호 확인"
-					type="password"
-					placeholder="비밀번호를 입력해주세요."
-					{...register("confirmPassword", { required: true })}
-				/>
-				{errors.confirmPassword && (
-					<p className="error">{errors.confirmPassword.message}</p>
-				)}
-				<Form.Input
-					text="닉네임"
-					type="text"
-					placeholder="닉네임을 입력해주세요."
-					{...register("nickname", { required: true })}
-				/>
-				{errors.nickname && <p className="error">{errors.nickname.message}</p>}
+			<form className={`${styles.container}`} onSubmit={handleSubmit(onSubmit)}>
 				<div>
+					<ImgInput name="profileImage" />
+					<ErrorMessage
+						errors={errors}
+						name="profileImage"
+						render={({ message }) => <p>{message}</p>}
+					/>
+				</div>
+				<div>
+					<Form.Input<SignUpType>
+						name="username"
+						title="아이디"
+						placeholder="아이디를 입력해주세요."
+						type="text"
+						className={
+							errors.username
+								? `${styles.input} ${styles.inputError}`
+								: styles.input
+						}
+					/>
+					{/* <div>{formState.errors.username?.message}</div> */}
+					<ErrorMessage
+						errors={errors}
+						name="username"
+						render={({ message }) => <p>{message}</p>}
+					/>
+				</div>
+				<div>
+					<Form.Input<SignUpType>
+						name="password"
+						title="비밀번호"
+						placeholder="비밀번호를 입력해주세요."
+						type="password"
+						className={
+							errors.password
+								? `${styles.input} ${styles.inputError}`
+								: styles.input
+						}
+					/>
+					<ErrorMessage
+						errors={errors}
+						name="password"
+						render={({ message }) => <p>{message}</p>}
+					/>
+				</div>
+				<div>
+					<Form.Input<SignUpType>
+						name="confirmPassword"
+						title="비밀번호 확인"
+						placeholder="비밀번호를 입력해주세요."
+						type="password"
+						className={
+							errors.confirmPassword
+								? `${styles.input} ${styles.inputError}`
+								: styles.input
+						}
+					/>
+					<ErrorMessage
+						errors={errors}
+						name="confirmPassword"
+						render={({ message }) => <p>{message}</p>}
+					/>
+				</div>
+				<div>
+					<Form.Input<SignUpType>
+						name="nickname"
+						title="닉네임"
+						placeholder="닉네임을 입력해주세요."
+						type="text"
+						className={
+							errors.nickname
+								? `${styles.input} ${styles.inputError}`
+								: styles.input
+						}
+					/>
+					<ErrorMessage
+						errors={errors}
+						name="nickname"
+						render={({ message }) => <p>{message}</p>}
+					/>
+				</div>
+				<div className={`${styles.genderWrap}`}>
 					<div>성별</div>
-					<Form.RadioButton
-						text="남성"
-						value="남성"
-						{...register("gender", { required: true })}
+					<div className={`${styles.genderBox}`}>
+						<Form.RadioButton name="gender" text="남성" value="남성" />
+						<Form.RadioButton name="gender" text="여성" value="여성" />
+						<Form.RadioButton name="gender" text="비공개" value="비공개" />
+					</div>
+					<ErrorMessage
+						errors={errors}
+						name="gender"
+						render={({ message }) => <p>{message}</p>}
 					/>
-					<Form.RadioButton
-						text="여성"
-						value="여성"
-						{...register("gender", { required: true })}
-					/>
-					<Form.RadioButton
-						text="비공개"
-						value="비공개"
-						{...register("gender", { required: true })}
-					/>
-					{errors.gender && <p className="error">{errors.gender.message}</p>}
 				</div>
-
 				<div>
-					<Form.Input
-						text="만 나이"
-						type="number"
+					<Form.Input<SignUpType>
+						name="age"
+						title="만 나이"
 						placeholder="나이를 입력해주세요."
-						{...register("age", { required: true })}
+						type="number"
+						className={
+							errors.age ? `${styles.input} ${styles.inputError}` : styles.input
+						}
 					/>
-					{errors.age && <p className="error">{errors.age.message}</p>}
+					<ErrorMessage
+						errors={errors}
+						name="age"
+						render={({ message }) => <p>{message}</p>}
+					/>
 				</div>
-				<TagCommon text="비타민" />
-				<Form.Button name="확인" type="submit" variant="dark" />
-			</Form>
+				<div className={`${styles.tagWrap}`}>
+					{tagData.map((tags) => (
+						<TagButton key={tags.id} text={tags.name} name="tag" />
+					))}
+					<ErrorMessage
+						errors={errors}
+						name="tag"
+						render={({ message }) => <p>{message}</p>}
+					/>
+				</div>
+				<Form.Button text="확인" type="submit" variant="dark" />
+			</form>
 			<DevTool control={control} />
 		</div>
 	);
