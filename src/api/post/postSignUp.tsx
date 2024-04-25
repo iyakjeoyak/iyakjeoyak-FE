4f2a77c795b5f24f66358a49c7bfd4dfda6ede11
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2024-04-25 17:30:32 +0900

    fix: signup api 작업

---
+++ src/api/post/postSignUp.tsx
@@ -0,0 +1,23 @@
+import { SignUpType } from "@/pages/signup/utils/signupValidation";
+import instance from "..";
+import { AxiosError, AxiosResponse } from "axios";
+
+export default async function postSignUp(data: SignUpType) {
+	try {
+		const response: AxiosResponse = await instance.post<SignUpType>(
+			"http://54.180.121.206:8080/user",
+			data,
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+			},
+		);
+		return response.data;
+	} catch (err) {
+		if (err instanceof AxiosError) {
+			console.error(err);
+			return err.response;
+		}
+	}
+}
--- src/hooks/queries/queryKeys.ts
+++ src/hooks/queries/queryKeys.ts
@@ -1,6 +1,7 @@
 const queryKeys = {
 	Medicines: () => ["medicines"],
 	ReviewsById: (id: number) => ["reviews", id],
+	SignUp: () => ["signUp"],
 };
 
 export default queryKeys;
