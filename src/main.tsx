69b9cfab4ee9e2be4c9b9a7a53a781ce372fba1d
hyunjinb <hyunjinb394@gmail.com>
2024-04-28 23:44:35 +0900

    Feat : toast 에러핸들링 & query cache 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import App from "./App.tsx";
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+	MutationCache,
+	QueryCache,
+	QueryClient,
+	QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ErrorBoundary } from "react-error-boundary";
 import Error from "@pages/feedback/UI/Error.tsx";
 import Loading from "./pages/feedback/Loading.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { showToast } from "./utils/ToastConfig.ts";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -16,6 +24,20 @@ const queryClient = new QueryClient({
 		},
 		mutations: {},
 	},
+	queryCache: new QueryCache({
+		onError: (error, query) => {
+			if (!query.meta?.ignoreToast) {
+				showToast({ type: "error", message: error.message });
+			}
+		},
+	}),
+	mutationCache: new MutationCache({
+		onError: (error, request, context, mutation) => {
+			if (!mutation.meta?.ignoreToast) {
+				showToast({ type: "error", message: error.message });
+			}
+		},
+	}),
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -24,6 +46,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 			<Suspense fallback={<Loading />}>
 				<QueryClientProvider client={queryClient}>
 					<App />
+					<ToastContainer />
 					<ReactQueryDevtools initialIsOpen={false} position="bottom" />;
 				</QueryClientProvider>
 			</Suspense>
