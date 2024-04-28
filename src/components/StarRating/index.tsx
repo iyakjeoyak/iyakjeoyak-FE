ddeb0ae744c5d5f861c7c08dfc6972db7a7c6d39
hyunjinb <hyunjinb394@gmail.com>
2024-04-29 01:19:54 +0900

    Refactor : star 계산 함수 외부로 뺌

--- src/components/StarRating/index.tsx
+++ src/components/StarRating/index.tsx
@@ -7,21 +7,26 @@ interface StarRatingProps {
 	size?: number;
 }
 
+const getStarRating = (filledStars: number, totalStars: number, size: number) => {
+	const fullStarsCount = Math.floor(filledStars); // 꽉 채워진 별 개수
+	const halffilledStar = filledStars % 1 >= 0.5; // 반 별 존재 여부
+	const emptyStarsCount = totalStars - fullStarsCount - (halffilledStar ? 1 : 0);
+  
+	const fullStars = Array(fullStarsCount).fill(<FaStar size={size} />);
+	const halfStars = halffilledStar ? [<FaStarHalfAlt size={size} />] : [];
+	const emptyStars = Array(emptyStarsCount).fill(<FaRegStar size={size} />);
+  
+	return [...fullStars, ...halfStars, ...emptyStars];
+  };
+
+
 const StarRating = ({
 	totalStars = 5,
 	filledStars,
 	size = 15,
 }: StarRatingProps) => {
-	const fullStarsCount = Math.floor(filledStars); // 꽉 채워진 별 개수
-	const halffilledStar = filledStars % 1 >= 0.5; // 반 별 존재여부
-	const emptyStarsCount =
-		totalStars - fullStarsCount - (halffilledStar ? 1 : 0);
 
-	const fullStars = Array(fullStarsCount).fill(<FaStar size={size} />);
-	const halfStars = halffilledStar ? [<FaStarHalfAlt size={size} />] : [];
-	const emptyStars = Array(emptyStarsCount).fill(<FaRegStar size={size} />);
-
-	const stars = [...fullStars, ...halfStars, ...emptyStars];
+	const stars = getStarRating(filledStars, totalStars, size)
 
 	return (
 		<div className={style.starRatingContainer}>
