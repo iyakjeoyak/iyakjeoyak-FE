040f6797b525a223b6e331edab3a6fe3ce6cc415
a-honey <jungahyeon0512@khu.ac.kr>
2024-04-30 19:30:44 +0900

    Error: 페이지 0 이후 불러오기 에러 및 타입 에러

--- src/pages/search/UI/MedicineCardList.tsx
+++ src/pages/search/UI/MedicineCardList.tsx
@@ -1,15 +1,16 @@
 import { MedicineItemType, ResponsePagenation } from "@/types";
 import { QueryFunction, QueryFunctionContext, QueryKey, useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useRef, useState } from "react";
 
 import MedicineCardItem from "./MedicineCardItem";
 import { TAPS_QUERIES } from "@/constants/TAPS";
 import TapBar from "@/components/TapBar";
+import axios from "@/api/axiosConfig";
 import getMedicines from "@/api/medicine/getMedicines";
 import medicineQueryOptions from "@/api/medicine";
 import styles from "../styles/MedicineCardList.module.scss";
 import { useNavigate } from "react-router-dom";
 import { useObserver } from "@/hooks/useObserver";
-import { useRef } from "react";
 
 const TAPS = [
 	{
@@ -33,30 +34,33 @@ interface MedicineQueryResponse {
 export default function MedicineCardList({toggleIsTagsModalOpen}:{toggleIsTagsModalOpen: ()=>void}) {
   const navigate = useNavigate();
   const bottom = useRef<HTMLDivElement>(null);
+
+  let currentPage = 0;
+  const PAGE_SIZE = 8;
   
-  const {data: {data: medicines,
-    // number,
-    // size,
-    // totalPages,
-    // totalElement,
-    // numberOfElement
-  }} = useQuery({...medicineQueryOptions.getMedicines({page:0, size: 8})});
-
-  const queryFn= async (context: any) => {
-    const data = await getMedicines(context.pageParam as number, 8);
-    return data;
+  const getMedicines = async ({
+    pageParam = { page: 0, size: PAGE_SIZE }
+  }: {
+    pageParam?: { page: number; size: number };
+  }) => {
+    const { page, size } = pageParam;
+    const res = await axios.get<ResponsePagenation<MedicineItemType>>(
+      `/medicine?page=${page}&size=${size}`
+    );
+    return res.data;
   };
-
   
   const {isLoading, data, hasNextPage, fetchNextPage}=useInfiniteQuery<ResponsePagenation<MedicineItemType>>({
     queryKey: ['medicine', 'medicines'],
-    queryFn,
-    getNextPageParam: (currentPage: any) => {
-    const nextPage = currentPage.page +1;
-    return nextPage < currentPage.total_pages ? null : nextPage;
-  },
-  initialPageParam: 0,
-})
+    queryFn: getMedicines,
+    getNextPageParam: () => {
+      console.log(currentPage)
+      const nextPage = currentPage + 1;
+     return { page: nextPage, size: PAGE_SIZE };
+    },
+    initialPageParam: { page: 0, size: PAGE_SIZE }, 
+    },
+  )
 
   const handleTapClick = (tapValue: string) => {
     toggleIsTagsModalOpen();
@@ -78,7 +82,7 @@ export default function MedicineCardList({toggleIsTagsModalOpen}:{toggleIsTagsMo
     <article>
     <TapBar taps={TAPS} onClick={handleTapClick} />
 		<div className={styles.container}>
-			{medicines && medicines.map((medicineItem)=><MedicineCardItem key={medicineItem.id} medicineItem={medicineItem}/>)}
+			{data?.pages[0]?.data.map((medicineItem)=><MedicineCardItem key={medicineItem.id} medicineItem={medicineItem}/>)}
 		  {isLoading ? <div>로딩중</div> : <div ref={bottom} />}
     </div>
     </article>
