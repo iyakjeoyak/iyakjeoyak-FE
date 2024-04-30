89f58829fd6a7fedacea9047773c33c0e7c2fe31
a-honey <jungahyeon0512@khu.ac.kr>
2024-04-30 17:37:18 +0900

    Error: useObserver 및 useInfinteQuery but 에러

--- src/api/medicine/getMedicines.ts
+++ src/api/medicine/getMedicines.ts
@@ -4,7 +4,7 @@ import { MedicineItemType, ResponsePagenation } from "@/types";
 
 import axios from "../axiosConfig";
 
-export default async function getMedicines({page, size}:{page:number, size:number}) {
+export default async function getMedicines(page: number, size: number) {
 	const response = await axios.get<ResponsePagenation<MedicineItemType>>(`/medicine?page=${page}&size=${size}`);
 	return response.data;
 }
