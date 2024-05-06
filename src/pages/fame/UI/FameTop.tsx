cd1af935890f39e794ca9719b79139c6469de3e9
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2024-05-06 16:21:29 +0900

    feat:fame 작업

--- src/pages/fame/UI/FameTop.tsx
+++ src/pages/fame/UI/FameTop.tsx
@@ -1,15 +1,36 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import CrownSvg from "@/assets/icons/Crown";
 import styles from "@/pages/fame/styles/FameTop.module.scss";
+
 export function FameTop() {
 	return (
-		<ul className={styles.contaier}>
-			<li>
+		<Swiper
+			className={styles.contaier}
+			spaceBetween={50}
+			slidesPerView={1}
+			centeredSlides={true}
+			loop={true}
+			navigation={true}
+			autoplay={{ delay: 3000 }}
+			modules={[Pagination, Navigation]}
+		>
+			<SwiperSlide className={`${styles.wrap}`}>
 				<div className={`${styles.circle} ${styles.gold} `}></div>
-			</li>
-
-			<li>
+				<CrownSvg fill="#ffd700" />
+			</SwiperSlide>
+			<SwiperSlide className={`${styles.wrap}`}>
 				<div className={`${styles.circle} ${styles.silver} `}></div>
+				<CrownSvg fill="#c0c0c0" />
+			</SwiperSlide>
+			<SwiperSlide className={`${styles.wrap}`}>
 				<div className={`${styles.circle} ${styles.bronze} `}></div>
-			</li>
-		</ul>
+				<CrownSvg fill="#cd7f32" />
+			</SwiperSlide>
+		</Swiper>
 	);
 }
