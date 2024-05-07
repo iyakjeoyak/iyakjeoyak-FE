f68a2d0a0b5e6df124d002352e06cc65aa8d1324
hyunjinb <hyunjinb394@gmail.com>
2024-05-08 01:19:35 +0900

    Fix : 브라우저 동작 확인

--- src/pages/userinfo/UI/Supplement/SupplementModal.tsx
+++ src/pages/userinfo/UI/Supplement/SupplementModal.tsx
@@ -1,6 +1,7 @@
 import SupplementDetailView from "./SupplementDetail";
 import SupplementEditForm from "./SupplementEditForm";
 import { SupplementInfo } from "../../userInfoType";
+import { supplementRecords as data } from "../../utils/mockData";
 import { useState } from "react";
 
 interface SupplementDetailModalProps {
@@ -27,43 +28,43 @@ const formInitialValues: SupplementInfo = {
 	img: undefined,
 };
 
-// const SupplementModal = ({ itemId }: SupplementDetailModalProps) => {
-// 	const [supplementData, setSupplementData] = useState<SupplementFormValues>(
-// 		data.mySupplements[itemId - 1] || formInitialValues,
-// 	);
+const SupplementModal = ({ itemId }: SupplementDetailModalProps) => {
+	const [supplementData, setSupplementData] = useState<SupplementFormValues>(
+		data.mySupplements[itemId - 1] || formInitialValues,
+	);
 
-// 	console.log(supplementData);
-// 	const [isEditing, setIsEditing] = useState(false);
+	console.log(supplementData);
+	const [isEditing, setIsEditing] = useState(false);
 
-// 	const handleEditClick = () => {
-// 		setIsEditing(true);
-// 	};
+	const handleEditClick = () => {
+		setIsEditing(true);
+	};
 
-// 	const onSubmit = (data: SupplementInfo) => {
-// 		console.log("폼제출", data);
-// 		setSupplementData(data);
-// 		setIsEditing(false);
-// 	};
+	const onSubmit = (data: SupplementInfo) => {
+		console.log("폼제출", data);
+		setSupplementData(data);
+		setIsEditing(false);
+	};
 
-// 	return (
-// 		<article>
-// 			{!isEditing ? (
-// 				<SupplementDetailView
-// 					supplementData={supplementData}
-// 					onEdit={handleEditClick}
-// 				/>
-// 			) : (
-// 				<SupplementEditForm
-// 					formInitialValues={
-// 						supplementData?.mySupplementId === 0
-// 							? formInitialValues
-// 							: supplementData
-// 					}
-// 					onSubmit={onSubmit}
-// 				/>
-// 			)}
-// 		</article>
-// 	);
-// };
+	return (
+		<article>
+			{!isEditing ? (
+				<SupplementDetailView
+					supplementData={supplementData}
+					onEdit={handleEditClick}
+				/>
+			) : (
+				<SupplementEditForm
+					formInitialValues={
+						supplementData?.mySupplementId === 0
+							? formInitialValues
+							: supplementData
+					}
+					onSubmit={onSubmit}
+				/>
+			)}
+		</article>
+	);
+};
 
-// export default SupplementModal;
+export default SupplementModal;
