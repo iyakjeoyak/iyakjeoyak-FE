3536bae2d6cc475901993c0630ba4d91b0984ae8
hyunjinb <hyunjinb394@gmail.com>
2024-05-08 13:48:04 +0900

    Feat: 지도 사용자 위치 버튼 추가

---
+++ src/pages/map/UI/MapSearch.tsx
@@ -0,0 +1,14 @@
+// export default function MapSearch() {
+
+//     const [keywordSearchResult, setKeywordSearchResult] = useState<
+//     KeywordResultItemType[]
+// >([]);
+
+// return(
+//     <>
+//     <section>
+
+//     </>
+// )
+
+// }
--- src/pages/map/UI/index.ts
+++ src/pages/map/UI/index.ts
@@ -1,3 +1,4 @@
 export { default as Map } from "./Map";
 export { default as MapDetail } from "./MapDetail";
+export { default as CurrentLocationButton } from "./CurrentLocationButton";
 export { default as LikedPharmacy } from "./LikedPharmacy";
