72eaba5a87f494db27c00f22976f90ab5f4e9e97
a-honey <jungahyeon0512@khu.ac.kr>
2024-05-15 22:21:14 +0900

    Fix: 테스트 코드 방식 변경

--- src/components/StarRating/index.test.ts
+++ src/components/StarRating/index.test.ts
@@ -1,35 +1,25 @@
 import { countStar } from ".";
 
 describe("getStarRating 함수 테스트 in StarRating", () => {
-	it("올바른 채워진 별, 반 별, 빈 별의 수를 객체로 반환한다.", () => {
-		const testCases = [
-			{
-				star: 4.5,
-				totalStars: 5,
-				expected: { fullStars: 4, halfStar: true, emptyStars: 0 },
-			},
-			{
-				star: 3.7,
-				totalStars: 5,
-				expected: { fullStars: 3, halfStar: true, emptyStars: 1 },
-			},
-			{
-				star: 2.2,
-				totalStars: 5,
-				expected: { fullStars: 2, halfStar: false, emptyStars: 3 },
-			},
-		];
-
-		testCases.forEach((test) => {
-			const { star, totalStars, expected } = test;
-			const { fullStarsCount, halffilledStar, emptyStarsCount } = countStar(
-				star,
-				totalStars,
-			);
-
-			expect(fullStarsCount).toEqual(expected.fullStars);
-			expect(halffilledStar).toEqual(expected.halfStar);
-			expect(emptyStarsCount).toEqual(expected.emptyStars);
+	test("0.5 단위 평점인 경우", () => {
+		expect(countStar(4.5, 5)).toStrictEqual({
+			emptyStarsCount: 0,
+			fullStarsCount: 4,
+			halffilledStar: true,
+		});
+	});
+	test("반올림이면 올림 단위 평점인 경우", () => {
+		expect(countStar(3.7, 5)).toStrictEqual({
+			emptyStarsCount: 1,
+			fullStarsCount: 3,
+			halffilledStar: true,
+		});
+	});
+	test("반올림이면내림 단위 평점인 경우", () => {
+		expect(countStar(2.2, 5)).toStrictEqual({
+			emptyStarsCount: 3,
+			fullStarsCount: 2,
+			halffilledStar: false,
 		});
 	});
 });
